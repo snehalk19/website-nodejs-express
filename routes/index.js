5d2d4ea0db4339a426edc1be89f10567d923474d
Snehal <snehalvkhot19@gmail.com>
2024-04-27 16:37:57 -0500

    Added middleware

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const speakersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
+const router = express.Router();
+
+module.exports = (params) => {
+  const { speakersService } = params;
+
+  router.get('/', async (request, response) => {
+    const topSpeakers = await speakersService.getList();
+    response.render('pages/index', { pageTitle: 'Welcome', template: 'index', topSpeakers });
+  });
+
+  router.use('/speakers', speakersRoute(params));
+  router.use('/feedback', feedbackRoute(params));
+  return router;
+};
